92867f254090106759cc3cb133d7fa336c26fd2a
Jonas Diemer <diemer@ida.ing.tu-bs.de>
2012-02-23 17:51:20 +0100

    hgignore the _build folder

---
+++ doc/index.rst
@@ -0,0 +1,35 @@
+.. pycpa documentation master file, created by
+   sphinx-quickstart on Mon Jan 30 14:17:05 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome
+=================================
+
+.. figure:: pycpa.png
+   :align:   right
+
+pyCPA is a pragmatic Python implementation of Compositional Performance Analysis
+(aka the SymTA/S approach provided by `Symtavision <http://www.symtavision.com>`_) 
+used for research in worst-case timing analysis.
+Unlike the commercial SymTA/S tool, pyCPA is not intended for commercial-grade use
+and does not guarantee correctness of the implementation.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   intro
+   modules
+   
+A examples and design concept must go here as well
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ examples/smff_loader.py
@@ -0,0 +1,70 @@
+"""
+| Copyright (C) 2012 Philip Axer
+| TU Braunschweig, Germany
+| All rights reserved. 
+| See LICENSE file for copyright and license details.
+
+:Authors:
+         - Philip Axer
+
+Description
+-----------
+
+SMFF Loader/Annotation example
+"""
+
+
+import os
+import string
+
+from pycpa import analysis
+from pycpa import smff_loader
+from pycpa import graph
+from pycpa import options
+
+def smff_test(file, outfile, plot, verbose):
+
+    print "loading", file
+    loader = smff_loader.SMFFLoader()
+    s = loader.parse(file)
+
+
+    if plot == True:
+        # graph the smff system
+        graph_file = string.replace(os.path.basename(file), ".xml", "") + ".pdf"
+        graph.graph_system(s, filename=graph_file)
+
+    # analyze the system
+    analysis.analyze_system(s)
+
+    # print some analysis results
+    if verbose == True:
+        print("Result:")
+        print(s)
+        for r in sorted(s.resources, key=str):
+            print "results for resource %s" % r.name
+            for t in sorted(r.tasks, key=str):
+                print("%s - %d " % (str(t.name) , t.wcrt))
+
+    if outfile is not None:
+        # backannotate the xml
+        loader.annotate_results()
+
+        # write it
+        loader.write(filename=outfile)
+
+if __name__ == "__main__":
+    # this is necessary because the file is also called from the regression test suite
+    default_file = os.path.dirname(os.path.realpath(__file__)) + "/smff_system.xml"
+    #default_outfile = os.path.dirname(os.path.realpath(__file__)) + "/smff_system_annotated.xml"
+
+    options.parser.add_argument('--file', '-f', type=str, default=default_file,
+                    help='File to load.')
+    options.parser.add_argument('--ofile', '-of', type=str, default=None,
+                    help='annotated output xml')
+    options.parser.add_argument('--graph', '-g', action='store_true',
+                    help='Graph the system, file will be saved to FILE.pdf. Where FILE is the input xml.')
+
+    options.init_pycpa()
+
+    smff_test(options.opts.file, options.opts.ofile, options.opts.graph, options.opts.verbose)
