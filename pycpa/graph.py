d8265553ed78f0e6608774d1a62f073e83d95c8c
Jonas Diemer <diemer@ida.ing.tu-bs.de>
2013-01-08 18:17:41 +0100

    renamed some functions, fixed some warnings, added some todos

---
+++ pycpa/graph.py
@@ -0,0 +1,133 @@
+"""
+| Copyright (C) 2007-2012 Jonas Diemer, Philip Axer
+| TU Braunschweig, Germany
+| All rights reserved.
+| See LICENSE file for copyright and license details.
+
+:Authors:
+         - Jonas Diemer
+         - Philip Axer
+
+Description
+-----------
+
+This module contains methods to plot task/architecture graphs of your system
+
+"""
+from __future__ import absolute_import
+from . import model
+
+try:
+    import pygraphviz
+except ImportError:
+    print "Sorry, you don't have the pygraphviz module installed."
+    print "Please install or reconfigure pygraphviz"
+    print "and try again."
+
+
+
+def get_junction_name(j):
+    name = 'Junction ' + j.name + ":"
+    for p in j.prev_tasks:
+        name += p.name + ","
+    return name
+
+
+def graph_system(s, filename=None, layout='dot',
+                 empty_resources=False, short_tasks=False,
+                 exec_times=False,
+                 sched_param=False,
+                 rankdir='LR',
+                 show=False):
+    """
+    Return a graph of the system
+
+    :param s: the system
+    :type s: model.System
+    :param filename:  if not None, the graph is plotted to this file
+    :param layout: graphviz layout algorithm (default l'dot' works best with hierarchical graphs)
+    :param empty_resources:  Plot resources that have no tasks assigned
+    :param short_tasks: Label tasks using "T_nn" instead of their potentially long name
+    :param exec_times: Show execution times for each tasks
+    :param sched_param: Show scheduling parameter for each task
+    :param rankdir: Layout option for graphviz
+    :param show: Show plot
+    :type show: boolean
+    :rtype: None
+    """
+
+    g = pygraphviz.AGraph(directed='true', compound='true',
+                          rankdir=rankdir,
+                          remincross='true',
+                          ordering='out'
+                          )
+
+    # first, create all nodes
+    task_num = 0
+    elen = 10
+
+    for r in s.resources:
+        if len(r.tasks) == 0 and not empty_resources:
+            continue  # dont plot resources without tasks
+
+        if g.has_node(r.name):
+            print "graph_system warning: duplicate resource", r.name
+        g.add_node(r.name, color='#aaaacc', shape='none')
+        res_tasks = [r.name]
+        for t in r.tasks:
+            if g.has_node(t.name):
+                print "graph_system warning: duplicate task", t.name
+            if short_tasks:
+                lab = "T_" + str(task_num)
+                task_num += 1
+            else:
+                lab = t.name
+            if exec_times:
+                lab += '(%g,%g)' % (t.bcet, t.wcet)
+            if sched_param:
+                lab += ' param: %s' % (str(t.scheduling_parameter))
+            g.add_node(t.name, label=str(lab))
+            res_tasks.append(t.name)
+            if t.mutex is not None:
+                g.add_node(t.mutex.name, color='#aaccaa', shape='hexagon')
+            for nt in t.next_tasks:
+                if isinstance(nt, model.Junction):
+                    g.add_node(get_junction_name(nt), label=nt.mode, shape='diamond')
+
+
+        g.add_subgraph(res_tasks, str("cluster_" + r.name))
+
+    # now come the connections
+
+    for r in s.resources:
+        for t in r.tasks:
+            for nt in t.next_tasks:
+                if isinstance(nt, model.Junction):
+                    g.add_edge(t.name, get_junction_name(nt), len=elen)  # edge to junction
+                    for jnt in nt.next_tasks:  # edges from junction
+                        g.add_edge(get_junction_name(nt), jnt.name, len=elen, constraint='True')
+                else:
+                    g.add_edge(t.name, nt.name, len=elen, constraint='True')
+
+            if t.mutex is not None:
+                g.add_edge(t.name, t.mutex.name, color='#aaccaa', len=1)
+
+            if t.prev_task is None:
+                g.add_node(str(t.in_event_model), len=10 * elen)
+                g.add_edge(str(t.in_event_model), t.name, constraint='True', style='dashed')
+
+
+    g.layout(layout)
+
+
+    if filename is not None:
+        g.draw(filename)
+
+    if show:
+        try:
+            g.draw(format='xlib')
+        except IOError:
+            pass
+
+    return g
+
---
+++ pycpa/util.py
@@ -0,0 +1,293 @@
+"""
+| Copyright (C) 2011, 2012 Philip Axer
+| TU Braunschweig, Germany
+| All rights reserved.
+| See LICENSE file for copyright and license details.
+
+:Authors:
+         - Philip Axer
+         - Jonas Diemer
+
+Description
+-----------
+
+Various utility functions
+"""
+
+from __future__ import absolute_import
+
+import fractions
+import logging
+from collections import deque
+
+logger = logging.getLogger("pycpa")
+
+# time bases
+ps = 1000000000000
+ns = 1000000000
+us = 1000000
+ms = 1000
+s = 1
+
+
+def get_next_tasks(task):
+    """ return the list of next tasks for task object.
+    required for _breadth_first_search """
+    return task.next_tasks
+
+
+def breadth_first_search(task, func=None, get_reachable_tasks=get_next_tasks):
+    """ returns a set of nodes (tasks) which is reachable
+    starting from the starting task.
+    calls func on the first discover of a task.
+
+    get_reachable_tasks(task) specifies a function which returns all tasks
+    considered immediately reachable for a given task.
+    """
+    marked = set()
+    queue = deque()
+
+    queue.append(task)
+    marked.add(task)
+
+    if func is not None:
+        func(task)
+
+    while len(queue) > 0:
+        v = queue.popleft()
+        for e in get_reachable_tasks(v):
+            if e not in marked:
+                if func is not None:
+                    func(task)
+                marked.add(e)
+                queue.append(e)
+    return marked
+
+
+def generate_distance_map(system):
+    """ Precomputes a distance-map for all tasks in the system.
+    """
+    dist = dict()
+    for r in system.resources:
+        for t in r.tasks:
+            dist[t] = dijkstra(t)
+    return dist
+
+def dijkstra(source):
+    """ Calculates a distance-map from the source node
+    based on the dijkstra algorithm
+    The edge weight is 1 for all linked tasks
+    """
+    dist = dict()
+    previous = dict()
+
+    # since we don't have a global view on the graph, we aquire a set of all
+    # nodes using BFS
+    nodes = breadth_first_search(source)
+
+    for v in nodes:
+        dist[v] = float('inf')
+        previous[v] = None
+
+    # init source
+    dist[source] = 0
+
+    # working set of nodes to revisit
+    Q = nodes.copy()
+
+    while len(Q) > 0:
+        # get node with minimum distance
+        u = min(Q, key=lambda x: dist[x])
+
+        if dist[u] == float('inf'):
+            break  # all remaining vertices are inaccessible from source
+
+        Q.remove(u)
+
+        for v in u.next_tasks:  # where v has not yet been removed from Q.
+            alt = dist[u] + 1
+            if alt < dist[v]:
+                dist[v] = alt
+                previous[v] = u
+                Q.add(v)
+    return dist
+
+def additive_extension(additive_func, q, q_max):
+    """ Additive extension for event models.
+    Any sub- or super- additive function additive_func valid in the domain q \in [0, q_max]
+    is extended and the approximited value f(q) is returned.
+    NOTE: this cannot be directly used with delta curves, since they are "1-off",
+    thus if you supply a delta function to additive_func, note to add 1 and supply q-1.
+    e.g. util.additive_extension(lambda x: self.delta_min(x + 1), n - 1, q_max)
+    """
+    if q <= q_max:
+        return additive_func(q)
+    elif q == float('inf'):
+        return float('inf')
+    else:
+        div = q / q_max
+        rem = q % q_max
+        return div * additive_func(q_max) + additive_func(rem)
+
+
+def recursive_max_additive(additive_func, q, q_max, cache=None, cache_offset=1):
+    """ Sub-additive extension for event models.
+    Any sub-additive function additive_func valid in the domain q \in [0, q_max]
+    is extended and the value f(q) is returned.
+    It is optional to supply a cache dictionary for speedup.
+
+    NOTE: this cannot be directly used with delta curves, since they are "1-off",
+    thus if you supply a delta function to additive_func, note to add 1 and supply q-1.
+    e.g. ret = util.recursive_max_additive(lambda x: self.delta_min(x + 1), n - 1, q_max, self.delta_min_cache)
+
+    By default, the cache is filled according to the delta domain notion, so it can be used with delta-based event models.
+    To override this behavior, change the cache_offset parameter to zero
+    """
+    if cache is None:
+        cache = dict()
+    if q <= q_max:
+        return additive_func(q)
+    else:
+        ret = 0
+        for a in range(1, q_max + 1):
+            b = cache.get(q - a + cache_offset, None)  # cache is in delta domain (thus +1)
+            if b is None:
+                b = recursive_max_additive(additive_func, q - a, q_max, cache, cache_offset)
+                cache[q - a + cache_offset] = b
+            # print a, q - a, additive_func(a), b, additive_func(a) + b
+            ret = max(ret, additive_func(a) + b)
+        # print ret
+        return ret
+
+
+def recursive_min_additive(additive_func, q, q_max, cache=None, cache_offset=1):
+    """ Super-additive extension for event models.
+    Any additive function additive_func valid in the domain q \in [0, q_max]
+    is extended and the value f(q) is returned.
+    It is optional to supply a cache dictionary for speedup.
+
+    NOTE: this cannot be directly used with delta curves, since they are "1-off",
+    thus if you supply a delta function to additive_func, note to add 1 and supply q-1.
+    e.g. ret = util.recursive_min_additive(lambda x: self.delta_plus(x + 1), n - 1, q_max, self.delta_plus_cache)
+
+    By default, the cache is filled according to the delta domain notion, so it can be used with delta-based event models.
+    To override this behavior, change the cache_offset parameter to zero
+    """
+    if cache is None:
+        cache = dict()
+    if q <= q_max:
+        return additive_func(q)
+    else:
+        ret = float('inf')
+        for a in range(1, q_max + 1):
+            b = cache.get(q - a + cache_offset, None)  # cache is in delta domain (thus +1)
+            if b is None:
+                b = recursive_min_additive(additive_func, q - a, q_max, cache, cache_offset)
+                cache[q - a + cache_offset] = b
+            # print a, q - a, additive_func(a), b, additive_func(a) + b
+            ret = min(ret, additive_func(a) + b)
+        return ret
+
+
+def str_to_time_base(unit):
+    """ Return the time base for the string """
+    conversion = {'s': s, 'ms': ms, 'us': us, 'ns': ns, 'ps': ps}
+    if unit in conversion:
+        return conversion[unit]
+    else:
+        raise ValueError
+
+
+def time_base_to_str(t):
+    """ Return the time base for the string """
+    conversion = {s: 's', ms: 'ms', us: 'us', ns: 'ns', ps: 'ps'}
+    if t in conversion:
+        return conversion[t]
+    else:
+        raise ValueError
+
+
+def calculate_base_time(frequencies):
+    common_timebase = LCM(frequencies)
+    if common_timebase > ps:
+        error_msg = "high base-time value! consider using ps instead"
+        logger.error(error_msg)
+    return common_timebase
+
+
+def cycles_to_time(value, freq, base_time, rounding="ceil"):
+    """ Converts the cycle/bittimes to an absolute time in base_time
+    """
+    scaler = fractions.Fraction(base_time, freq)
+    value = fractions.Fraction(value)
+    if rounding == "ceil":
+        return int(fractions.math.ceil(value * scaler))
+    elif rounding == "floor":
+        return int(fractions.math.floor(value * scaler))
+    else:
+        raise NotImplementedError("rounding %s not supported" % rounding)
+
+
+def time_to_time(value, base_in, base_out, rounding="ceil"):
+    """ Convert an absolute time given in base_in
+    to another absolute time given in base_out
+    """
+    scaler = fractions.Fraction(base_out) / fractions.Fraction(base_in)
+    if rounding == "ceil":
+        return int(fractions.math.ceil(value * scaler))
+    elif rounding == "floor":
+        return int(fractions.math.floor(value * scaler))
+    else:
+        raise NotImplementedError("rounding %s not supported" % rounding)
+
+
+def time_to_cycles(value, freq, base_time, rounding="ceil"):
+    """ Converts an absolute time given in
+    the base_time domain into cycles
+    """
+    scaler = fractions.Fraction(base_time, freq)
+    value = fractions.Fraction(value)
+    if rounding == "ceil":
+        return int(fractions.math.ceil(value / scaler))
+    elif rounding == "floor":
+        return int(fractions.math.floor(value / scaler))
+    else:
+        raise NotImplementedError("rounding %s not supported" % rounding)
+
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    return fractions.gcd(a, b)
+
+def lcm(a, b):
+    """ Return lowest common multiple."""
+    return (a * b) / gcd(a, b)
+
+
+def GCD(terms):
+    """ Return gcd of a list of numbers."""
+    return reduce(fractions.gcd, terms)
+
+
+def LCM(terms):
+    """Return lcm of a list of numbers."""
+    return reduce(lcm, terms)
+
+
+def combinations_with_replacement(iterable, r):
+    """combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC """
+    # number items returned:  (n+r-1)! / r! / (n-1)!
+    pool = tuple(iterable)
+    n = len(pool)
+    if not n and r:
+        return
+    indices = [0] * r
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(list(range(r))):
+            if indices[i] != n - 1:
+                break
+        else:
+            return
+        indices[i:] = [indices[i] + 1] * (r - i)
+        yield tuple(pool[i] for i in indices)
