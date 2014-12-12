a63e1c5cb10ab8c809817a99372a1827c745ba06
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2014-12-12 15:23:22 +0100

    add "hook" method to Task for updating the execution time

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+| Copyright (C) 2012 Philip Axer
+| TU Braunschweig, Germany
+| All rights reserved
+
+:Authors:
+         - Philip Axer
+
+Description
+-----------
+
+Setup
+"""
+
+
+from setuptools import setup
+
+setup(name='pycpa',
+      version='1.0',
+      description='pyCPA - a python implementation of compositional performance analysis',
+      author='Jonas Diemer, Philip Axer',
+      author_email='{axer, diemer}@ida.ing.tu-bs,de',
+      url='http://code.google.com/p/pycpa',
+      license="MIT",
+      packages=['pycpa'],
+      install_requires=[ 'argparse'],
+      extras_require={'plot': 'matplotlib',
+                      'trace': 'numpy',
+                      'topology_plot' : 'pygraphviz',
+                      'gantt_charts' : 'simpy',
+                      'doc' : 'sphinx'}
+     )
